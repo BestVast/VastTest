a8f1a831ac06695ad974709310dba36976d87aed
anlianghao <>
2018-10-17 15:31:15 +0800

    runtime动态添加方法，传参，获取返回值

--- VastTest/VastTest/KVCTest/KVCTestViewController.m
+++ VastTest/VastTest/KVCTest/KVCTestViewController.m
@@ -8,6 +8,7 @@
 
 #import "KVCTestViewController.h"
 #import "KVCDog.h"
+#import <objc/runtime.h>
 @interface KVCTestViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *kvcTestBtn;
 
@@ -21,6 +22,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     _dog = [[KVCDog alloc] init];
+    
+    id i = [self performSelector:@selector(foo:) withObject:@"123"];
+    DLog(@"%@", i);
 }
 - (IBAction)clickKVCTestButton:(id)sender {
     UIButton *btn = sender;
@@ -33,6 +37,19 @@
     DLog(@"%@", [_dog valueForKey:@"isName"]);
     DLog(@"%@", [_dog valueForKey:@"_isName"]);
     DLog(@"%@", [_dog dictionaryWithValuesForKeys:@[@"isName", @"_isName"]]);
+}
+
+
++ (BOOL)resolveInstanceMethod:(SEL)sel {
+    if (sel == @selector(foo:)) {
+        class_addMethod([self class], sel, (IMP)foo1, "");
+        return YES;
+    }
+    return [super resolveInstanceMethod:sel];
+}
+id foo1(id obj, SEL _cmd, NSString *aaa) {
+    NSLog(@"22222-%@, %@", obj, aaa);
+    return @(5);
 }
 
 @end
